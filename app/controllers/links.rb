5a9388bd37796ccb047d9a62c4e93694c654f71e
Felipe Talavera <felipe.talavera@gmail.com>
2008-12-13 23:00:36 +0100

    comment controller filled

--- app/controllers/links.rb
+++ app/controllers/links.rb
@@ -2,21 +2,24 @@ class Links < Application
   
   # GET /links
   def index
+    @links = Link.all
     render
   end
 
   # GET /links/:id
   def show
-    render
+    @link = Link.find params[:id]
   end
 
   # GET /links/new
   def new
+    @link = Link.new
     render
   end
 
   # POST /links
   def create
+    @link = Link.new(:title => params[:title], :url => params[:url], :description => params[:description])
     render
   end
 
