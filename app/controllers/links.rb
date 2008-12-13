f6a6dfac4813c711636652781d5d93309edf8cfc
mort <manuel@simplelogica.net>
2008-12-13 20:22:19 +0100

    Recurso Link

---
+++ app/controllers/links.rb
@@ -0,0 +1,42 @@
+class Links < Application
+  
+  # GET /links
+  def index
+    render
+  end
+
+  # GET /links/:id
+  def show
+    render
+  end
+
+  # GET /links/new
+  def new
+    render
+  end
+
+  # GET /links/:id/edit
+  def edit
+    render
+  end
+
+  # GET /links/:id/delete
+  def delete
+    render
+  end
+
+  # POST /links
+  def create
+    render
+  end
+
+  # PUT /links/:id
+  def update
+    render
+  end
+
+  # DELETE /links/:id
+  def destroy
+    render
+  end
+end
--- config/router.rb
+++ config/router.rb
@@ -27,6 +27,7 @@
 
 Merb.logger.info("Compiling routes...")
 Merb::Router.prepare do
+  resources :links
   # RESTful routes
   # resources :posts
 
