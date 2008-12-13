f0498db2aedcd0cd168378ef08b021647211bdc7
javier <javier.ramirez.gomara@gmail.com>
2008-12-13 22:22:20 +0100

    conflicto con init rb

--- config/init.rb
+++ config/init.rb
@@ -1,18 +1,21 @@
 # Go to http://wiki.merbivore.com/pages/init-rb
  
 # Specify a specific version of a dependency
-# dependency "RedCloth", "> 3.0"
-
-#  use_orm :none
+ 
+ # dependency "flype-merb_markaby"
+ 
+# require "lib/merb_markaby"
+ 
+# use_orm :none
 use_test :rspec
-use_template_engine :erb
+use_template_engine :markaby
  
 Merb::Config.use do |c|
   c[:use_mutex] = false
-  c[:session_store] = 'cookie'  # can also be 'memory', 'memcache', 'container', 'datamapper
+  c[:session_store] = 'cookie' # can also be 'memory', 'memcache', 'container', 'datamapper
   
   # cookie session store configuration
-  c[:session_secret_key]  = '196b36c109f0c173dd420dbbd1d4b32a522487d9'  # required for cookie session store
+  c[:session_secret_key] = '196b36c109f0c173dd420dbbd1d4b32a522487d9' # required for cookie session store
   c[:session_id_key] = '_mummy_session_id' # cookie session id key, defaults to "_session_id"
 end
  
